f276fd607a01182957ef3afbe962660999f210e8
Nira00A <biswasarin341@gmail.com>
2026-04-11 23:46:54 +0530

    chore(Prisma-Tutorial): learned how to make prisma client and init prisma

--- Prisma-Tutorial/src/app.service.ts
+++ Prisma-Tutorial/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getHello(): Promise<string> {
     return 'Hello World!';
+  }
+
+  async getUsers(): Promise<any[]> {
+    return this.prisma.user.findMany();
   }
 }
